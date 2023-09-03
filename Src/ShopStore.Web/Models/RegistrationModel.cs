e73f2f5a201db84dd6368eb7bd78e1870b3b8966
Borhan Uddin Amin <borhanuddinamin@gmail.com>
2023-09-03 19:46:45 +0600

    re added login

---
+++ Src/ShopStore.Web/Models/RegistrationModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ShopStore.Web.Models
+{
+    public class RegistrationModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string  Password { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
